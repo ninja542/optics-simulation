41b787145d0888d6215a21a77538e080f4c34a3c
ninja542 <theroboticcyborg@gmail.com>
2017-11-05 21:49:32 -0500

    added dragging but not perfectly

--- optics.js
+++ optics.js
@@ -1,10 +1,11 @@
-d3.select("#convex-lens").call(d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended));
-function dragstarted(d){
-	d3.select(this).raise().classed("active", true);
+d3.select("#convex-lens").call(d3.drag().subject(subject).on("drag", dragmove));
+function dragmove(){
+	var x = d3.event.x;
+	var y = d3.event.y;
+	d3.select(this).attr("transform", "translate("+x+","+y+")");
 }
-function dragged(d){
-	d3.select(this).attr("x", d.x = d3.event.x).attr("y", d.y = d3.event.y);
-}
-function dragended(d){
-	d3.select(this).classed("active", false);
+function subject(d){
+	var t = d3.select(this);
+	return {x: t.attr("x"), y: t.attr("y")};
+	// return {x: d3.event.x, y: d3.event.y};
 }
